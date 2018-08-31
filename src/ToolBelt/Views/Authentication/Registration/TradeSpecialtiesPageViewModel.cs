7434983d424723cac58a50fcd1bcf84e377ef4ef
Eugene Pawlik <eugene.pawlik@gmail.com>
2018-08-30 22:29:21 -0500

    Show a message when no specialties are selected.
    Refs #6

--- src/ToolBelt/Views/Authentication/Registration/TradeSpecialtiesPageViewModel.cs
+++ src/ToolBelt/Views/Authentication/Registration/TradeSpecialtiesPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Navigation;
+using Prism.Services;
 using ReactiveUI;
 using System.Linq;
 using System.Reactive.Linq;
@@ -6,9 +7,12 @@ using ToolBelt.ViewModels;
 
 namespace ToolBelt.Views.Authentication.Registration
 {
+    // TODO: Most of this page should be moved into a content view rather than a page.  Then we could share it across the registration and profile editing pages.
     public class TradeSpecialtiesPageViewModel : BaseViewModel
     {
-        public TradeSpecialtiesPageViewModel(INavigationService navigationService) : base(navigationService)
+        public TradeSpecialtiesPageViewModel(
+            INavigationService navigationService,
+            IPageDialogService dialogService) : base(navigationService)
         {
             Title = "Trade Specialties";
 
@@ -30,7 +34,14 @@ namespace ToolBelt.Views.Authentication.Registration
 
             Next = ReactiveCommand.CreateFromTask(async () =>
             {
+                // if there are no items selected, they can't move on.  Must select at least one specialty
+                if (!Items.Any(item => item.IsSelected))
+                {
+                    await dialogService.DisplayAlertAsync("Error", "You must select at least one trade specialty", "OK").ConfigureAwait(false);
+                }
+
                 // TODO: ...
+
             });
 
             Items.AddRange(
